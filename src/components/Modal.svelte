47f749384668285cff49ce30ece3a27a90d64b14
kawsaramin101 <71335212+kawsaramin101@users.noreply.github.com>
2022-03-29 11:35:44 +0600

    Add files via upload

---
+++ src/components/Modal.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let showModal = false;
+    export let message = "I don't have any message, maybe the coder messed up";
+    export let isPromo = false;
+</script>
+
+{#if showModal }
+<div class="backdrop" class:promo={isPromo} on:click|self>
+    <div class="modal">
+        <button on:click>Close Modal</button>
+        <p>{message}</p>
+        <slot name="title"></slot>
+        <slot></slot>
+    </div>
+</div>
+{/if }
+
+<style>
+    .backdrop{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+  }
+  .modal{
+    padding: 1em;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;;
+    text-align: center;
+    background: white;
+  }
+  .promo .modal{
+    background: crimson;
+    color: white;
+  }
+  
+</style>
---
+++ src/components/addNinjaForm.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { createEventDispatcher} from "svelte"
+    
+    let dispatch = createEventDispatcher();
+    
+    let name;
+    let beltColour;
+    let age;
+    let skills = [];
+    
+    function handleSubmit() {
+        const ninja = {
+            name,
+            beltColour: beltColor,
+            age,
+            skills,
+            id: Math.random()
+        };
+        dispatch("addNinja", ninja);
+    };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" bind:value={name} placeholder="Name" />
+    <input type="number" bind:value={age} placeholder="Age" />
+    <label>Belt Color:</label>
+    <select bind:value={beltColor}>
+        <option value="black">Black</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+    </select>
+    <label>Skills:</label>
+    <input type="checkbox" bind:group={skills} value="fighting"/>Fighting<br/>
+    <input type="checkbox" bind:group={skills} value="sneaking"/>Sneaking<br/>
+    <input type="checkbox" bind:group={skills} value="running"/>Running<br/>
+    <br/>
+    <button>Add Ninja</button>
+</form>
+
+<style>
+    
+</style>
+
