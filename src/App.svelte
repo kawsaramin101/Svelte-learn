d67791cba866ba1f972ae072a9ef5d3eceb986fa
kawsaramin101 <71335212+kawsaramin101@users.noreply.github.com>
2022-03-29 11:33:34 +0600

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+    //import Tut0 from "./tuts/Tut0.svelte";
+    import Tut1 from "./tuts/Tut1.svelte";
+
+</script>
+
+
+<main>
+
+    <Tut1 />
+
+</main>
+
+<style>
+    main {
+        text-align: center;
+       /* padding: 1em;*/
+        
+        margin: 0 auto;
+    }
+/*
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+@media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }*/
+</style>
